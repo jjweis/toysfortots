d2bb7e612bfd9be624c8c73323e467ca77236aff
jjweis <john@jjweis.com>
2015-12-18 23:15:07 -0500

    JQuery Mobile Testing

---
+++ database.js
@@ -0,0 +1,246 @@
+window.COUNTIES = {
+	"Allen" : {
+		"coordinator" : [
+			{
+			"name" : "Sunny Helstrom",
+			"email" : "fortwaynetoysfortots@gmail.com",
+			"phone" : "260-438-6603"
+			},
+		{
+			"name" : "Melissa Coats",
+			"email" : "mdc_tft@yahoo.com",
+			"phone" : "260-908-2022"
+		}
+	],
+			"location" : [
+				{
+					"name" : "Kroger",
+					"address" : "1125 W State Blvd",
+					"city" : "Fort Wayne",
+					"state" : "IN",
+					"zip" : "46808",
+					"startDate" : "11/2/2015",
+					"endDate" : "12/7/2015"
+				},
+				{
+					"name" : "Kroger",
+					"address" : "218 E Pettit Ave",
+					"city" : "Fort Wayne",
+					"state" : "IN",
+					"zip" :	"IN",
+					"startDate" : "11/2/2015",
+					"endDate" : "12/7/2015"
+				}
+				]
+			},
+	"Adams" : {
+		"coordinator" : [
+			{
+			"name" : "Sunny Helstrom",
+			"email" : "fortwaynetoysfortots@gmail.com",
+			"phone" : "260-438-6603"
+			},
+			{
+			"name" : "Melissa Coats",
+			"email" : "mdc_tft@yahoo.com",
+			"phone" : "260-908-2022"
+		}
+	]
+},
+	"Dekalb" : {
+		"coordinator" : [
+			{
+			"name" : "Sunny Helstrom",
+			"email" : "fortwaynetoysfortots@gmail.com",
+			"phone" : "260-438-6603"
+			},
+			{
+			"name" : "Melissa Coats",
+			"email" : "mdc_tft@yahoo.com",
+			"phone" : "260-908-2022"
+		}
+	]
+	},
+	"Huntington" : {
+		"coordinator" : [
+			{
+			"name" : "Sunny Helstrom",
+			"email" : "fortwaynetoysfortots@gmail.com",
+			"phone" : "260-438-6603"
+			},
+			{
+			"name" : "Melissa Coats",
+			"email" : "mdc_tft@yahoo.com",
+			"phone" : "260-908-2022"
+		}
+	]
+},
+	"Noble" : {
+		"coordinator" : [
+			{
+			"name" : "Sunny Helstrom",
+			"email" : "fortwaynetoysfortots@gmail.com",
+			"phone" : "260-438-6603"
+			},
+			{
+			"name" : "Melissa Coats",
+			"email" : "mdc_tft@yahoo.com",
+			"phone" : "260-908-2022"
+		}
+	]
+	},
+	"Steuben" : {
+		"coordinator" : [
+			{
+			"name" : "Sunny Helstrom",
+			"email" : "fortwaynetoysfortots@gmail.com",
+			"phone" : "260-438-6603"
+			},
+		 	{
+			"name" : "Melissa Coats",
+			"email" : "mdc_tft@yahoo.com",
+			"phone" : "260-908-2022"
+		}
+	]
+	},
+	"Wells" : {
+		"coordinator" : [
+			{
+			"name" : "Sunny Helstrom",
+			"email" : "fortwaynetoysfortots@gmail.com",
+			"phone" : "260-438-6603"
+			},
+			{
+			"name" : "Melissa Coats",
+			"email" : "mdc_tft@yahoo.com",
+			"phone" : "260-908-2022"
+		}
+	]
+	},
+	"Marion" : {
+		"coordinator" : [
+			{
+			"name" : "Ryan Boman",
+			"email" : "toysfortots.indianapolis@gmail.com",
+			"site" : "https://www.facebook.com/IndyToysforTots"
+			}
+		]
+		},
+	"Tippecanoe" : {
+		"coordinator" : [
+			{
+			"name" : "Ryan Boman",
+			"email" : "toysfortots.indianapolis@gmail.com",
+			"site" : "https://www.facebook.com/IndyToysforTots"
+			}
+		]
+		},
+	"Clinton" : {
+		"coordinator" : [
+			{
+			"name" : "Ryan Boman",
+			"email" : "toysfortots.indianapolis@gmail.com",
+			"site" : "https://www.facebook.com/IndyToysforTots"
+			}
+		]
+		},
+	"Montgomery" : {
+		"coordinator" : [
+			{
+			"name" : "Ryan Boman",
+			"email" : "toysfortots.indianapolis@gmail.com",
+			"site" : "https://www.facebook.com/IndyToysforTots"
+			}
+		]
+	},
+	"Boone" : {
+		"coordinator" : [
+			{
+			"name" : "Ryan Boman",
+			"email" : "toysfortots.indianapolis@gmail.com",
+			"site" : "https://www.facebook.com/IndyToysforTots"
+			}
+		]
+		},
+	"Hamilton" : {
+		"coordinator" : [
+			{
+			"name" : "Ryan Boman",
+			"email" : "toysfortots.indianapolis@gmail.com",
+			"site" : "https://www.facebook.com/IndyToysforTots"
+			}
+		]
+		},
+	"Putnam" : {
+		"coordinator" : [
+			{
+			"name" : "Ryan Boman",
+			"email" : "toysfortots.indianapolis@gmail.com",
+			"site" : "https://www.facebook.com/IndyToysforTots"
+			}
+		]
+		},
+	"Hendricks" : {
+		"coordinator" : [
+			{
+			"name" : "Ryan Boman",
+			"email" : "toysfortots.indianapolis@gmail.com",
+			"site" : "https://www.facebook.com/IndyToysforTots"
+			}
+		]
+		},
+	"Brown" : {
+		"coordinator" : [
+			{
+			"name" : "Ryan Boman",
+			"email" : "toysfortots.indianapolis@gmail.com",
+			"site" : "https://www.facebook.com/IndyToysforTots"
+			}
+		]
+		},
+	"Decatur" : {
+		"coordinator" : [
+			{
+			"name" : "Ryan Boman",
+			"email" : "toysfortots.indianapolis@gmail.com",
+			"site" : "https://www.facebook.com/IndyToysforTots"
+			}
+		]
+		},
+	"Morgan" : {
+		"coordinator" : [
+			{
+			"name" : "Ryan Boman",
+			"email" : "toysfortots.indianapolis@gmail.com",
+			"site" : "https://www.facebook.com/IndyToysforTots"
+			}
+		]
+		},
+	"Johnson" : {
+		"coordinator" : [
+			{
+			"name" : "Ryan Boman",
+			"email" : "toysfortots.indianapolis@gmail.com",
+			"site" : "https://www.facebook.com/IndyToysforTots"
+			}
+		]
+		},
+	"Owen" : {
+		"coordinator" : [
+			{
+			"name" : "Ryan Boman",
+			"email" : "toysfortots.indianapolis@gmail.com",
+			"site" : "https://www.facebook.com/IndyToysforTots"
+			}
+		]
+		},
+	"Monroe" : {
+		"coordinator" : [
+			{
+			"name" : "Ryan Boman",
+			"email" : "toysfortots.indianapolis@gmail.com",
+			"site" : "https://www.facebook.com/IndyToysforTots"
+			}
+		]
+		}
+	};
